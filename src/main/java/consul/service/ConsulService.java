a5ec5312806ec6b081792c669e98e747edb5f464
Jigar Joshi <jigarjm@gmail.com>
2015-08-14 13:14:05 -0700

    comments

--- src/main/java/consul/service/ConsulService.java
+++ src/main/java/consul/service/ConsulService.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * Uses consul's REST API and exposes certain functionality
  * Created by jigar.joshi on 8/9/15.
  */
 public final class ConsulService {
