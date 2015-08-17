4169be464b7373835ef071d88152876030b6036e
Jigar Joshi <jigarjm@gmail.com>
2015-08-17 14:22:56 -0700

    adding licenses and legal stuff

--- src/main/java/org/elasticsearch/discovery/consul/ConsulDiscovery.java
+++ src/main/java/org/elasticsearch/discovery/consul/ConsulDiscovery.java
@@ -1,24 +1,15 @@
 /*
- * Copyright (c) 2015 Grant Rodgers
- *
- *     Permission is hereby granted, free of charge, to any person obtaining
- *     a copy of this software and associated documentation files (the "Software"),
- *     to deal in the Software without restriction, including without limitation
- *     the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *     and/or sell copies of the Software, and to permit persons to whom the Software
- *     is furnished to do so, subject to the following conditions:
- *
- *     The above copyright notice and this permission notice shall be included in
- *     all copies or substantial portions of the Software.
- *
- *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- *     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- *     OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+These files were modified by Lithium Technologies, Inc. and all such modifications are:
+Copyright © 2015 Lithium Technologies, Inc. All rights reserved subject to the terms of the MIT License below.
+Original files from the “Elasticsearch SRV discovery plugin” project, prior to modification by Lithium, are:
+
+Copyright (c) 2015 Grant Rodgers
+
+MIT License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 package org.elasticsearch.discovery.consul;
 
@@ -36,17 +27,15 @@ import org.elasticsearch.node.settings.NodeSettingsService;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
-/**
- *
- */
 public class ConsulDiscovery extends ZenDiscovery {
 
-    @Inject
-    public ConsulDiscovery(Settings settings, ClusterName clusterName, ThreadPool threadPool, TransportService transportService,
-                           ClusterService clusterService, NodeSettingsService nodeSettingsService, ZenPingService pingService,
-                           DiscoveryNodeService discoveryNodeService, DiscoverySettings discoverySettings,
-                           ElectMasterService electMasterService, DynamicSettings dynamicSettings) {
-        super(settings, clusterName, threadPool, transportService, clusterService, nodeSettingsService,
-                discoveryNodeService, pingService, electMasterService, discoverySettings, dynamicSettings);
-    }
+	@Inject
+
+	public ConsulDiscovery(Settings settings, ClusterName clusterName, ThreadPool threadPool, TransportService transportService,
+	                       ClusterService clusterService, NodeSettingsService nodeSettingsService, ZenPingService pingService,
+	                       DiscoveryNodeService discoveryNodeService, DiscoverySettings discoverySettings,
+	                       ElectMasterService electMasterService, DynamicSettings dynamicSettings) {
+		super(settings, clusterName, threadPool, transportService, clusterService, nodeSettingsService,
+				discoveryNodeService, pingService, electMasterService, discoverySettings, dynamicSettings);
+	}
 }
--- src/main/java/org/elasticsearch/discovery/consul/ConsulDiscoveryModule.java
+++ src/main/java/org/elasticsearch/discovery/consul/ConsulDiscoveryModule.java
@@ -1,23 +1,12 @@
 /*
- * Copyright (c) 2015 Grant Rodgers
- *
- *     Permission is hereby granted, free of charge, to any person obtaining
- *     a copy of this software and associated documentation files (the "Software"),
- *     to deal in the Software without restriction, including without limitation
- *     the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *     and/or sell copies of the Software, and to permit persons to whom the Software
- *     is furnished to do so, subject to the following conditions:
- *
- *     The above copyright notice and this permission notice shall be included in
- *     all copies or substantial portions of the Software.
- *
- *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- *     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- *     OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+These files were modified by Lithium Technologies, Inc. and all such modifications are:
+Copyright © 2015 Lithium Technologies, Inc. All rights reserved subject to the terms of the MIT License below.
+Original files from the “Elasticsearch SRV discovery plugin” project, prior to modification by Lithium, are:
+Copyright (c) 2015 Grant Rodgers
+MIT License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package org.elasticsearch.discovery.consul;
@@ -27,18 +16,16 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.discovery.Discovery;
 import org.elasticsearch.discovery.zen.ZenDiscoveryModule;
 
-/**
- *
- */
+
 public class ConsulDiscoveryModule extends ZenDiscoveryModule {
 
-    @Inject
-    public ConsulDiscoveryModule(Settings settings) {
-        addUnicastHostProvider(ConsulUnicastHostsProvider.class);
-    }
+	@Inject
+	public ConsulDiscoveryModule(Settings settings) {
+		addUnicastHostProvider(ConsulUnicastHostsProvider.class);
+	}
 
-    @Override
-    protected void bindDiscovery() {
-        bind(Discovery.class).to(ConsulDiscovery.class).asEagerSingleton();
-    }
+	@Override
+	protected void bindDiscovery() {
+		bind(Discovery.class).to(ConsulDiscovery.class).asEagerSingleton();
+	}
 }
